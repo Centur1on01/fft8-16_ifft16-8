f828df8e7ed3b9e0388469def718d484a0ead764
centur1on01 <for_gamesvova@mail.ru>
2023-05-23 22:24:28 +0300

    Добавьте файлы проекта.

---
+++ fft_ifft/reader_fft.m
@@ -0,0 +1,64 @@
+
+
+
+%spectre = spectre after fft
+%signal = signal after ifft
+%signalb4 = signal before fft
+%check = signal after ifft(test of 16 to 8 bit ifft)
+sineSpectre = fopen('check.txt');
+sSp = textscan(sineSpectre,'%f\t%f');
+
+
+plot(sSp{1},real(sSp{2}));
+%title('Спектр');
+
+fclose(sineSpectre);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
